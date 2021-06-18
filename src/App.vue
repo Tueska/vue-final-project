079c6dce7d1a40dc785743fad473c41f218b4af5
Tueska <github@tue.sk>
2021-06-18 15:05:49 +0200

    Added GameType and Game component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <Game :game="gameList[1]" /> -->
+  <p>{{ gameList }}</p>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { Options, Vue } from "vue-class-component";
+import axios from "axios";
+import Game from "./components/Game.vue";
+import GameType from "@/interfaces/GameType";
 
-export default defineComponent({
-  name: 'App',
+@Options({
   components: {
-    HelloWorld
+    Game,
+  },
+})
+export default class App extends Vue {
+  gameList: GameType[] | "Error" = [];
+  gameID = 0;
+
+  created(): void {
+    axios({
+      method: "GET",
+      url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
+      headers: {
+        "x-rapidapi-key": "d19335f5f2mshd18d3d2f7703cebp124fe3jsn36f8a0db2672",
+        "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+      },
+    })
+      .then((res) => {
+        this.gameList = res.data;
+      })
+      .catch(() => {
+        this.gameList = "Error";
+      });
+
+    console.log(this.gameList);
   }
-});
+
+  get article(): GameType | "Error" {
+    return this.gameList != "Error" ? this.gameList[this.gameID] : "Error";
+  }
+}
 </script>
 
 <style>
---
+++ src/components/Game.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ game.title }}</h1>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import GameType from "@/interfaces/GameType";
+import { PropType } from "node_modules/vue/dist/vue";
+
+@Options({
+  props: {
+    game: Object as PropType<GameType>,
+  },
+})
+export default class Game extends Vue {
+  game!: GameType;
+}
+</script>
+
+<style scoped></style>
